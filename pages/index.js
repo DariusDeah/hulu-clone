105d8caf756df62afd1b7de3f9120f24f08abc85
DariusDeah <dariusdeah21@gmail.com>
2022-07-20 08:26:29 -0600

    requests set up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
       <Head>Hulu 2.0</Head>
       {/* Header */}
       <Header />
-      {/* Nav */}
+      <Nav />
       {/* Results */}
     </div>
   );
