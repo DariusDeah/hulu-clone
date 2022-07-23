4f2de1e107c336182f91d400eeeb1a20331698b9
DariusDeah <dariusdeah21@gmail.com>
2022-07-22 22:40:07 -0600

    SignupForm updated

--- components/SignUpForm.jsx
+++ components/SignUpForm.jsx
@@ -23,86 +23,81 @@ function SignUpForm() {
         confirmPasswordRef.current.value
       )
     );
-
-    const validateIsValidName = (name) => name < 2;
-    const validateIsValidEmail = (email) => email.includes("@");
-    const validateIsValidPassword = (password) => password.length >= 8;
-    const validateIsValidConfirmPassword = (
-      originalPassword,
-      passwordConfirm
-    ) => passwordConfirm === originalPassword;
-
-    return (
-      <div className=" w-full flex text-black  justify-center my-8">
-        <form
-          className="flex w-3/4 flex-col bg-white rounded-lg p-4 md:p-14 group space-y-7 font-bold"
-          onSubmit={handleFormSubmit}
-        >
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            className="p-3 rounded-md bg-gray-200"
-            ref={nameRef}
-          />
-          {!isValidName && <p className="text-red-500 ">Invalid Name</p>}
-
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            name="email"
-            className="p-3 rounded-md bg-gray-200"
-            ref={emailRef}
-          />
-          {!isValidEmail && (
-            <p className="text-red-500 ">
-              Invalid Email, email must conatain @
-            </p>
-          )}
-
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            className="p-3 rounded-md bg-gray-200"
-            ref={passwordRef}
-          />
-          {!isValidPassword && (
-            <p className="text-red-500 ">
-              Invalid password, password must match rules
-            </p>
-          )}
-
-          <div>
-            <ul className="list-disc list-inside">
-              <p>password must:</p>
-              <li>be 8 charaters long at minimum</li>
-              <li>must include one special character "%#@!?"</li>
-              <li>not use any personal information</li>
-            </ul>
-          </div>
-
-          <label htmlFor="password-Confirm"> Confirm Password</label>
-          <input
-            type="password"
-            name="password-Confirm"
-            className="p-3 rounded-md bg-gray-200"
-            ref={confirmPasswordRef}
-          />
-          {!isValidConfirmPassword && (
-            <p className="text-red-500 ">passwords must match</p>
-          )}
-
-          <button
-            type="submit"
-            className="p-2 rounded-md bg-green-500  text-white font-bold"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-    );
   };
+  const validateIsValidName = (name) => name < 2;
+  const validateIsValidEmail = (email) => email.includes("@");
+  const validateIsValidPassword = (password) => password.length >= 8;
+  const validateIsValidConfirmPassword = (originalPassword, passwordConfirm) =>
+    passwordConfirm === originalPassword;
+
+  return (
+    <div className=" w-full  flex text-black p-5 justify-center my-8">
+      <form
+        className="flex md:w-2/3 flex-col bg-white rounded-lg p-4 md:p-14 group space-y-7 font-bold"
+        onSubmit={handleFormSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          className="p-3 rounded-md bg-gray-200"
+          ref={nameRef}
+        />
+        {!isValidName && <p className="text-red-500 ">Invalid Name</p>}
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          className="p-3 rounded-md bg-gray-200"
+          ref={emailRef}
+        />
+        {!isValidEmail && (
+          <p className="text-red-500 ">Invalid Email, email must conatain @</p>
+        )}
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          className="p-3 rounded-md bg-gray-200"
+          ref={passwordRef}
+        />
+        {!isValidPassword && (
+          <p className="text-red-500 ">
+            Invalid password, password must match rules
+          </p>
+        )}
+
+        <div>
+          <ul className="list-disc list-inside">
+            <p>password must:</p>
+            <li>be 8 charaters long at minimum</li>
+            <li>must include one special character "%#@!?"</li>
+            <li>not use any personal information</li>
+          </ul>
+        </div>
+
+        <label htmlFor="password-Confirm"> Confirm Password</label>
+        <input
+          type="password"
+          name="password-Confirm"
+          className="p-3 rounded-md bg-gray-200"
+          ref={confirmPasswordRef}
+        />
+        {!isValidConfirmPassword && (
+          <p className="text-red-500 ">passwords must match</p>
+        )}
+
+        <button
+          type="submit"
+          className="p-2 rounded-md bg-green-500  text-white font-bold"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpForm;
--- pages/signup.js
+++ pages/signup.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import { useState } from "react";
 import BundleDisplay from "../components/BundleDisplay";
 import HeaderItem from "../components/HeaderItem";
 import SignUpForm from "../components/SignUpForm";
 
 export default function SignUp() {
+  const [toggleSignupForm, setToggleSignupForm] = useState(false);
+
+  const handleToggleSignUpForm = () => {
+    setToggleSignupForm(true);
+  };
+
   return (
-    <div className="">
+    <div>
       <div className=" border-white border-b-2 p-2">
         {/* <Image
           className="object-contain"
@@ -18,14 +25,19 @@ export default function SignUp() {
         </h2>
       </div>
       <div className="flex  justify-center ">
-        <div className="border-white rounded-md justify-center   flex flex-col align-middle  ">
-          <BundleDisplay />
-          <SignUpForm />
+        <div className="border-white rounded-md justify-center w-full   flex flex-col align-middle  ">
+          {toggleSignupForm ? <SignUpForm /> : <BundleDisplay />}
           <div className="flex justify-center space-x-3 md:space-x-20">
-            <button className="bg-white hover:bg-green-500  text-black p-3 pr-12 pl-12 font-bold w-fit rounded-xl  ">
+            <button
+              className="bg-white hover:bg-green-500  text-black p-3 pr-12 pl-12 font-bold w-fit rounded-xl  "
+              onClick={handleToggleSignUpForm}
+            >
               Signup & Bundle
             </button>
-            <button className="bg-white hover:bg-green-500  text-black p-3 pr-12 pl-12 font-bold w-fit rounded-xl  ">
+            <button
+              className="bg-white hover:bg-green-500  text-black p-3 pr-12 pl-12 font-bold w-fit rounded-xl  "
+              onClick={handleToggleSignUpForm}
+            >
               Signup
             </button>
           </div>
